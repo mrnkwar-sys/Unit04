535bd13ab42a5fedcfacb8ae736bb7a8d858a297
Rei ヽ( ´￢`)ﾉ <mr.nkwar@iesnervion.es>
2026-03-12 14:41:17 +0100

    ♥♦♣♠•◘○7,♀☺!

--- src/boletin2/CuentaCorriente.java
+++ src/boletin2/CuentaCorriente.java
@@ -4,8 +4,18 @@ class CuentaCorriente {
 	private String dni;
 	private String name;
 	private double balance;
-	private String nationality;
+	enum Nationality{
+		español, extranjero
+	}
 	
+	Nationality nationality;
+	
+	CuentaCorriente(){
+		this.dni = "";
+		this.name = "";
+		this.balance = 0;
+		this.nationality = Nationality.español;
+	}
 	
 	CuentaCorriente (String dni, double balance){
 		this.dni = dni;
@@ -18,7 +28,7 @@ class CuentaCorriente {
 		this.balance = balance;
 	}
 	
-	CuentaCorriente (String dni, String name, double balance, String nationality){
+	CuentaCorriente (String dni, String name, double balance, Nationality nationality){
 		this.dni = dni;
 		this.name = name;
 		this.balance = balance;
@@ -37,7 +47,7 @@ class CuentaCorriente {
 		return balance;
 	}
 	
-	public String getNationality() {
+	public Nationality getNationality() {
 		return nationality;
 	}
 	
@@ -53,15 +63,66 @@ class CuentaCorriente {
 		this.balance = balance;
 	}
 	
-	public void setNationality(String nationality) {
-		if (nationality.equals("Española") || nationality.equals("Extranjera")) {
-			this.nationality = nationality;
+	public boolean withdrawMoney(double balance, double quantityIntroduced) {
+		//The boolean that will tell if it is possible to withdraw money from the bank
+		boolean withdrawMoney;
+		
+		//If there is enough money inside the account to withdraw some, the boolean will be true and the balance will be reduced
+		if (balance >= quantityIntroduced) {
+			this.balance -= quantityIntroduced;
+			withdrawMoney = true;
 		} else {
-			this.nationality = "Otra";
+			withdrawMoney = false;
 		}
+		
+		return withdrawMoney;
 	}
 	
-	public void sacarDinero(double balance) {
+	public boolean depositMoney(double balance, double quantityIntroduced) {
+		//The boolean that will tell if it is possible to deposit the quantityIntroduced in the bank 
+		boolean depositMoney;
 		
+		if (quantityIntroduced > 0) {
+			this.balance += quantityIntroduced;
+			depositMoney = true;
+		} else {
+			depositMoney = false;
+		}
+		
+		return depositMoney;
+	}
+	
+	public String toStringDni() {
+		String chain = "DNI: " + dni;
+		return chain;
+	}
+	
+	public String toStringName() {
+		String chain = "Nombre del usuario: " + name;
+		return chain;
+	}
+	
+	public String toStringBalance() {
+		String chain = "Dinero disponible en la cuenta: " + balance;
+		return chain;
+	}
+	
+	public String toStringNationality() {
+		String chain = "Nacionalidad del usuario: " + nationality;
+		return chain;
+	}
+	
+	public boolean equals(Object objeto) {
+		//The boolean that will indicate if the banks accounts are different or not
+		boolean sameBank = false;
+		
+		//The cast
+		CuentaCorriente otraCuenta = (CuentaCorriente) objeto;
+		
+		if (this.name.equals(otraCuenta.name) && this.dni.equals(otraCuenta.dni)) {
+			sameBank = true;
+		}
+		
+		return sameBank;
 	}
 }
--- src/boletin2/MainCuentaCorriente.java
+++ src/boletin2/MainCuentaCorriente.java
@@ -1,10 +1,28 @@
 package boletin2;
 
+import java.util.Scanner;
+
+import boletin2.CuentaCorriente.Nationality;
+
 public class MainCuentaCorriente {
 
 	public static void main(String[] args) {
+		//Create Scanner
+		Scanner sc = new Scanner(System.in);
 		
-
+		//Both bank accounts
+		CuentaCorriente account = new CuentaCorriente();
+		CuentaCorriente anotherAccount = new CuentaCorriente();
+		
+		//The user has to introduce their data
+		System.out.println("Introduzca su nombre:");
+		//The user has to choose one the options from the menu
+		System.out.println("1. SACAR DINERO");
+		System.out.println("2. INGRESAR DINERO");
+		System.out.println("3. INFORMACIÓN DE LA CUENTA");
+		System.out.println("4. COMPROBAR SI DOS CUENTAS SON IGUALES");
+		
 	}
+	
 
 }
